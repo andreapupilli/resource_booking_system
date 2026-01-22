b18e2c41a942f21e71633b5d5127cff6765355dd
Andrea Pupilli <andrea.pupilli@studenti.unicam.it>
2026-01-22 15:08:24 +0100

    Test: enforce test profile and H2 config for CI

--- src/test/java/it/unicam/resourcebooking/ResourceBookingApplicationTests.java
+++ src/test/java/it/unicam/resourcebooking/ResourceBookingApplicationTests.java
@@ -2,12 +2,12 @@ package it.unicam.resourcebooking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
+@ActiveProfiles("test")
 class ResourceBookingApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
